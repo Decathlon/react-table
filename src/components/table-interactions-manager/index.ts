37ac8a08c720dac5ea1f4e9a6b1cdf8ece5a34d0
amen <amen.souissi@decathlon.com>
2020-08-12 15:16:19 +0200

    chore(Table): fix exports

--- src/components/table-interactions-manager/index.ts
+++ src/components/table-interactions-manager/index.ts
@@ -7,6 +7,9 @@ export * from "./column-visibility-controller";
 export { default as ColumnIdScrollController } from "./column-id-scroll-controller";
 export * from "./column-id-scroll-controller";
 
+export { default as FixedColumnController } from "./fixed-column-controller";
+export * from "./fixed-column-controller";
+
 export { default as TableInteractionsManager } from "./table-interactions-manager";
 export * from "./table-interactions-manager";
 
